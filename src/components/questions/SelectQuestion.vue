7efa3dddc0599c2d62ed1437966afb1558e861e5
pelevin-d-y <natpuot1992@gmail.com>
2019-07-04 15:43:44 +0300

    select card start

---
+++ src/components/questions/SelectQuestion.vue
@@ -0,0 +1,16 @@
+<template>
+  <BaseQuestion :questionCard="questionCard">
+  </BaseQuestion>
+</template>
+
+<script>
+import BaseQuestion from '@/components/questions/BaseQuestion'
+
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
