2058b4216c3fb29d47b4a9bf1aed272eea577618
pelevin-d-y <natpuot1992@gmail.com>
2019-07-31 14:30:21 +0300

    Fix: port init

--- server.js
+++ server.js
@@ -2,12 +2,13 @@ const express = require('express')
 const app = express()
 app.use(express.static('dist/')) // absolute or relative to CWD
 // TODO: remove switch block when develop subdomain will be irrelevant !!!
+let port = 0
 switch(__dirname) {
     case '/home/deployer/uic-deploy':
-        let port = 1337
+        port = 1337
         break
     case '/home/deployer/uic-develop-deploy':
-        let port = 1338
+        port = 1338
         break
     default:
         break
